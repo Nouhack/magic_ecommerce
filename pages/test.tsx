3b9d045eba7fdd235cd84f8ea35773a569ff46cd
Nouhack <nouh.saiche@gmail.com>
2023-01-01 21:21:06 +0100

    init 7

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,23 +1,11 @@
 import { Component } from 'react'
-import { attributes, react as HomeContent } from '../content/products.md';
 
 export default class Home extends Component {
   render() {
-    let { title, cats } = attributes;
+    //let { title, cats } = attributes;
     return (
       <>
-        <article>
-          <h1>{title}</h1>
-          <HomeContent />
-          <ul>
-            {cats.map((cat: any, k: number) => (
-              <li key={k}>
-                <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-              </li>
-            ))}
-          </ul>
-        </article>
+        nouh
       </>
     )
   }
