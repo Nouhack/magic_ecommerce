b87375f0510debf7f6fe99900742b6f196251004
Nouhack <nouh.saiche@gmail.com>
2023-01-02 20:19:17 +0100

    dfsf

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
   webpack: (cfg) => {
     cfg.module.rules.push({
       test: /\.md$/,
-      use: 'frontmatter-markdown-loader',
+      loader: 'frontmatter-markdown-loader',
       options: { mode: ['react-component'] }
     })
     return cfg
